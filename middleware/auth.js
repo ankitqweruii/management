2a1d3eb73de322159979e8ccb6657bb9c51c94b7
ankitqweruii <ankitsharma@Ankits-MacBook-Air.local>
2025-06-26 11:24:59 -0400

    add console

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 module.exports = function (req, res, next) {
   const token = req.header('Authorization')?.split(" ")[1];
   if (!token) return res.status(401).json({ msg: "No token, authorization denied" });
-
+  console.log("add console in code")
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded.user;
